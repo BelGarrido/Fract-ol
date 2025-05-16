ba6730793019c81f7fcd557f0b958c10a22916d4
Ana Garrido Diaz <anagarri@c3r5s1.42malaga.com>
2025-05-16 12:54:41 +0200

    fractol-monitor issues

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: anagarri <anagarri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 11:40:26 by anagarri          #+#    #+#             */
-/*   Updated: 2025/05/15 14:36:29 by anagarri         ###   ########.fr       */
+/*   Updated: 2025/05/16 12:50:50 by anagarri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,8 @@ typedef struct s_fractal
 	int 		type;
 	double		julia_cx;
 	double		julia_cy;
+	int32_t 	*monitor_heigth;
+	int32_t		*monitor_width;
 	//	mandelbrot = 1
 	//	julia = 2
 	} 			t_fractal;
@@ -48,7 +50,7 @@ typedef struct	s_complex
 /*string utils*/
 int				ft_strncmp(char *s1, char *s2, size_t n);
 void			ft_putstr_fd(char *s, int fd);
-double				ft_atoi(const char *s);
+double			ft_atof(const char *s);
 
 /*math*/
 double			rescale_map(double number, double new_min, double new_max, double old_max);
@@ -59,9 +61,8 @@ void			build_julia(int x, int y, t_fractal *fractal);
 void			esc_hook(mlx_key_data_t keydata, void *parameter);
 void			zoom_hook(double xdelta, double ydelta, void *parameter);
 
-//void draw_square (t_fractal fractal, uint32_t *pixel_buffer);
 int				draw_fractal (t_fractal *fractal);
 unsigned int	get_color_iterations(int i, int iterations);
-//void render_frame(void *param);
+void			fractal_init(t_fractal *fractal);
 
 #endif
